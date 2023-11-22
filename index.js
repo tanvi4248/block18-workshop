0e831cf7cb4ed11183c2774ef84cd43566b58652
tanvi4248 <tanvi@GWTN141-10>
2023-11-22 12:08:39 -0600

    Add psudocode

---
+++ index.js
@@ -0,0 +1,38 @@
+//Test specs for Unit test
+// 1) A function called "multiplication" that returns the product of the two input numbers.
+// step 1: Make a function multiplication. 
+// step 2: Declare variables x,y and product. 
+// step 3: Give integer numbers to x and y.
+// step 4: calculate product = x * y
+// step 5: return product
+
+// Expect multiplication(8, 9) to be a number
+// Expect multiplication(8, 9) to be equal to 72
+// Expect multiplication(8, "d") to be an error
+
+//2)
+// Step 1: Declare two arrays of integers array1,array2,array3 and array4.
+// Step 2: Give values to array1 and array2.
+// step 3: call function concatOdds(array1,array2).
+// step 4: calculate array3 = array1.concat(array2).
+// step 5: return array3.
+// step 6: check if array3[i] % 2 == 1 if yes
+// step 7: store values into array4.
+
+
+//Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be integer numbers.
+//Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be [-1, 1, 3, 9, 15].
+
+//Unexpected input 
+//Expect concatOdds([0.9, 3.14, 8.5], [9, 1, 1, 1, 4, 15, -1]) to be an error
+// Unexpected inputs are non-integer numbers.
+
+//Functional test
+//When user click on "cart" without adding product to cart they should be shown some error (your cart is empty.) and prompted to sign in or sign up.
+//Verify that users can log in to an existing account to proceed with checkout.
+//check if users can create a new account during the checkout process.
+//check users enter information is correct or not.
+//check if user enter mandatory shipping information.
+//Verify that user can select a preferred shipping method.
+//Test different payment methods like credit,paypa,apple pay etc.
+
